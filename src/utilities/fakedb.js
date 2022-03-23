f7452fce85865477086a36bfded18ac3a4b87767
rahathossain3 <rahathossain210@gmail.com>
2022-03-23 10:38:55 +0600

    local stoage data added

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,16 +1,39 @@
 //use local storage to manage cart data
 
 const addToDb = (id) => {
-    const quantity = localStorage.getItem(id);
+    //declare empty object  ---empty object ullek korle o cole na korle o cole
+
+    let shoppingCart = {};
+
+    //get the shopping cart
+    const storedCart = localStorage.getItem('shopping-cart');
+    // jodi paoya jay
+    if (storedCart) {
+        // console.log(storedCart);
+        shoppingCart = JSON.parse(storedCart);
+    }
+    // else {
+    //     shoppingCart = {};
+    // }
+
+    //set item from shopping cart in array
+    const quantity = shoppingCart[id];
+    //set item if it exists
     if (quantity) {
-        console.log('already Exists');
-        const newQuantity = parseInt(quantity) + 1;
-        localStorage.setItem(id, newQuantity);
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
+        // localStorage.setItem(id, newQuantity);
     }
     else {
-        console.log('save item');
-        localStorage.setItem(id, 1);
+        // new item added when item is not exists
+        shoppingCart[id] = 1;
+        // localStorage.setItem(id, 1);
     }
+
+
+    //set object in local storage 
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
+
 
 }
 
